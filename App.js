3a69b799c23842ec0544e29c74f441a1fa723a65
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-12-14 12:23:10 +0300

    firebase auth setup

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import OnboardingScreen from "./screens/OnboardingScreen";
